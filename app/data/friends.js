dfd891a613992ef4a28fbf8b27790123b44022d0
Jennifer Chan <jenniferchan2267@yahoo.com>
2020-03-09 13:11:10 -0700

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,48 +1,48 @@
 var friends = [
     {
         name: "Jennifer Aniston",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [4, 4, 2, 1, 1, 1, 2, 2, 4, 5]
     },
     {
         name: "Courteney Cox",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [5, 5, 1, 5, 1, 1, 3, 5, 1, 3]
     },
     {
         name: "David Schwimmer",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [1, 5, 5, 5, 5, 5, 5, 5, 1, 1]
     },
     {
         name: "Lisa Kudrow",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [5, 1, 1, 1, 1, 1, 1, 1, 5, 5]
     },
     {
         name: "Matthew Perry",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [4, 5, 5, 5, 2, 1, 5, 5, 1, 1]
     },
     {
         name: "Matt LeBlanc",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
     },
     {
         name: "Reese Witherspoon",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [5, 1, 5, 1, 4, 3, 5, 1, 1, 1]
     },
     {
         name: "Maggie Wheeler",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
 
     },
     {
         name: "Paul Rudd",
-        photo: "",
+        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }
 ];
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,54 @@
+  // LOAD DATA 
+
+const friendData = require("../data/friends.js");
+
+  // ROUTING
+
+module.exports = function(app) {
+  // API GET Request
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+
+  // API POST Request
+
+  app.post("/api/friends", function(req, res) {
+    
+    const name = req.body.name;
+    const photo = req.body.photo; 
+    const newScores = req.body.scores; 
+    const newEntry = {
+      "name": name,
+      "photo": photo,
+      "scores": []
+    };
+
+    const newScoresArr = newEntry.scores;
+    newScores.forEach(function(index) {
+      newScoresArr.push(parseInt(newScores[index]));
+    });
+    console.log(newScoresArr);
+
+    const scoreDiffArr = [];
+    for (let i = 0; i < friendData.length; i++) {
+      let savedScores = friendData[i].scores; 
+      let difference = 0;
+      for (let j = 0; j < newScoresArr.length; j++) {
+        difference += Math.abs(newScoresArr[j] - savedScores[j]);
+      }
+      scoreDiffArr.push(difference);
+    }
+    console.log(scoreDiffArr);
+
+    let closest = Math.min.apply(null, scoreDiffArr);
+    let match = scoreDiffArr.indexOf(closest);
+    console.log(match);
+
+    let matchedFriend = friendData[match];
+    res.json(matchedFriend);
+    friendData.push(newEntry);
+  });
+
+}
