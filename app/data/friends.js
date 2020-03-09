9189710db3d52d8c94a92fe59607e06b39e3a96f
Jennifer Chan <jenniferchan2267@yahoo.com>
2020-03-09 09:49:08 -0700

    update friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,50 @@
 var friends = [
     {
-        name: "Test1",
-        photo:
-            "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg/440px-Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        name: "Jennifer Aniston",
+        photo: "",
+        scores: [4, 4, 2, 1, 1, 1, 2, 2, 4, 5]
     },
+    {
+        name: "Courteney Cox",
+        photo: "",
+        scores: [5, 5, 1, 5, 1, 1, 3, 5, 1, 3]
+    },
+    {
+        name: "David Schwimmer",
+        photo: "",
+        scores: [1, 5, 5, 5, 5, 5, 5, 5, 1, 1]
+    },
+    {
+        name: "Lisa Kudrow",
+        photo: "",
+        scores: [5, 1, 1, 1, 1, 1, 1, 1, 5, 5]
+    },
+    {
+        name: "Matthew Perry",
+        photo: "",
+        scores: [4, 5, 5, 5, 2, 1, 5, 5, 1, 1]
+    },
+    {
+        name: "Matt LeBlanc",
+        photo: "",
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        name: "Reese Witherspoon",
+        photo: "",
+        scores: [5, 1, 5, 1, 4, 3, 5, 1, 1, 1]
+    },
+    {
+        name: "Maggie Wheeler",
+        photo: "",
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+
+    },
+    {
+        name: "Paul Rudd",
+        photo: "",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    }
 ];
 
 module.exports = friends;
