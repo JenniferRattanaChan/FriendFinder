6a02db9117a1246c9822115b129322431cdad701
Jennifer Chan <jenniferchan2267@yahoo.com>
2020-03-09 02:41:05 -0700

    add data to friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friends = [
+    {
+        name: "Test1",
+        photo:
+            "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg/440px-Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+];
+
+module.exports = friends;
