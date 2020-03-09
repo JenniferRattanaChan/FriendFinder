3d46dacb5da7eb41b83c3a28adf1e225910a2cb4
Jennifer Chan <jenniferchan2267@yahoo.com>
2020-03-09 13:53:05 -0700

    final edit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,48 +1,48 @@
 var friends = [
     {
         name: "Jennifer Aniston",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/eLiqVGrDAGRTdN5Tna/giphy.gif",
         scores: [4, 4, 2, 1, 1, 1, 2, 2, 4, 5]
     },
     {
         name: "Courteney Cox",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/QaSnEODWw0PH3QuIgE/giphy.gif",
         scores: [5, 5, 1, 5, 1, 1, 3, 5, 1, 3]
     },
     {
         name: "David Schwimmer",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/jQt7hOEcISZxAUNtgQ/giphy.gif",
         scores: [1, 5, 5, 5, 5, 5, 5, 5, 1, 1]
     },
     {
         name: "Lisa Kudrow",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/JSvgjnJawWYqZJGsVU/giphy.gif",
         scores: [5, 1, 1, 1, 1, 1, 1, 1, 5, 5]
     },
     {
         name: "Matthew Perry",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/d631TwECR94r9zlCB3/giphy.gif",
         scores: [4, 5, 5, 5, 2, 1, 5, 5, 1, 1]
     },
     {
         name: "Matt LeBlanc",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/mGPjrn9F6h8RKUIn6J/giphy.gif",
         scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
     },
     {
         name: "Reese Witherspoon",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/M8uM4L6LCrnMQ8U72C/giphy.gif",
         scores: [5, 1, 5, 1, 4, 3, 5, 1, 1, 1]
     },
     {
         name: "Maggie Wheeler",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        photo: "https://media.giphy.com/media/hXOc9DJufLxpzo82zq/giphy.gif",
         scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
 
     },
     {
-        name: "Paul Rudd",
-        photo: "https://images7.alphacoders.com/406/thumb-350-406068.jpg",
+        name: "James Michael Tyler",
+        photo: "https://media.giphy.com/media/J4mri0PpAifBrhgHXu/giphy.gif",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }
 ];
